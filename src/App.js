4871f80ff8a7585498f6e89354d99f13460d6343
Damon Allen <damon.nicholas.allen@live.com>
2023-08-01 18:55:47 -0400

    added header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
+import picture1 from './images/106193373.jpg'
 
 function App() {
   return (
     <div className="App">
       <header>
+      <img src={picture1} className='picture1'/>
         <h1>Damon's Site</h1>
+        <h2>A learning Frontend Web Dev</h2>
       </header>
+      <div className='bio'>
+        <p></p>
+      </div>
     </div>
   );
 }
