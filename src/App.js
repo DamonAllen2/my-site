7b0b86e689d1aa0c50dcf2acc543a41f7f4125ca
Damon Allen <damon.nicholas.allen@live.com>
2023-08-04 15:09:11 -0400

    added a bio

--- src/App.js
+++ src/App.js
@@ -5,12 +5,19 @@ function App() {
   return (
     <div className="App">
       <header>
-      <img src={picture1} className='picture1'/>
+      <img src={picture1} className='picture1' alt='of me'/>
         <h1>Damon's Site</h1>
-        <h2>A learning Frontend Web Dev</h2>
+        <h2>A learning Frontend</h2>
+        <h2>Web Developer</h2>
+        <h2>At Bloomtech</h2>
       </header>
       <div className='bio'>
-        <p></p>
+        <p>
+          As a little introduction, my name is Damon Allen.<br>
+          </br>I am from Ohio. I started coding in 2022.<br>
+          </br>I hope to get a starting career in coding, and possibly different things in the future.<br>
+          </br>Some things that I enjoy doing is playing guitar, and editing videos using Adobe Premiere Pro.
+        </p>
       </div>
     </div>
   );
