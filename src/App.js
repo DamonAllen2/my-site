e5b4b21f90368d78a7bd5ebb6733f461e48cc73a
Damon Allen <damon.nicholas.allen@live.com>
2023-08-08 13:12:34 -0400

    added more pet images

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
 import picture1 from './images/106193373.jpg'
-import foxy from './images/foxy.JPG'
-import fred from './images/fred.jpg'
-import shaggy from './images/scooby.jpg'
-import scooby from './images/shaggy.jpg'
+import foxy from './images/foxy.png'
+import fred from './images/fred.png'
+import shaggy from './images/scooby.png'
+import scooby from './images/shaggy.png'
+import foxandsoph from './images/foxandsoph.png'
 
 function App() {
   return (
@@ -16,14 +17,23 @@ function App() {
         <h2>At Bloomtech</h2>
       </header>
       <div className='bio'>
+        <div className='pet-container'>
       <img src={foxy} className='foxy' alt='My dog Foxy'/>
       <img src={fred} className='fred' alt='My cat Fred'/>
+      </div>
+      <div className='bio-text'>
         <p>
           As a little introduction, my name is Damon Allen.<br>
           </br>I am from Ohio. I started coding in 2022.<br>
           </br>I hope to get a starting career in coding, and possibly different things in the future.<br>
           </br>Some things that I enjoy doing is playing guitar, and editing videos using Adobe Premiere Pro.
         </p>
+        </div>
+      </div>
+      <div className='pet-pictures'>
+      <img src={scooby} className='pets' alt='My cat Scooby'/>
+      <img src={shaggy} className='pets' alt='My cat Shaggy'/>
+      <img src={foxandsoph} className='pets' alt='Both of my dogs'/>
       </div>
     </div>
   );
